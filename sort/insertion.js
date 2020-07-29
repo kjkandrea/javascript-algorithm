fc623feb69fd4108e2d7b4236d4e61297c0dae52
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-29 17:41:23 +0900

    Update insertion.js

--- sort/insertion.js
+++ sort/insertion.js
@@ -1,30 +1,16 @@
 // 삽입 정렬
 
-const arr = [ 6, 5, 1, 2, 4, 3 ]
+function insertionSort(arr) {
+  let temp, j; // 5
+  for ( let i = 1; i < arr.length; i++ ) {
+    temp = arr[i]
 
-function insertionSort(array) {
-  let i = 1
-  let temp, j
-
-  // outer llop
-  // 정렬된 배열을 유지할때 시작 사이즈를 2로 설정 ( 1 -> n )
-  for ( i; i < array.length; i++ ) {
-
-    temp = array[i]
-
-    console.log('outer loop %s / array : %s / temp : %s', i, array, temp)
-
-    // inner loop
-    // 정렬된 배열을 끝까지 탐색을 안했고 & 현재의 값 보다 더 작으면 -> 왼쪽으로 이동하라
-    for ( j = i - 1; j >= 0 && temp < array[j]; j-- ) {
-
-      array[j + 1] = array[j]
-
-      console.log('inner loop %s / array : %s / temp : %s', j, array, temp)
+    for ( j = i - 1; j >= 0 && temp < arr[j]; j-- ) {
+      arr[j + 1] = arr[j] // [6, 6, 1, 2, 4, 3]
     }
-    array[j + 1] = temp
+    arr[j + 1] = temp // [5, 6, 1, 2, 4, 3]
   }
-  return array
+  return arr
 }
 
-console.log(insertionSort(arr))
+console.log(insertionSort([ 6, 5, 1, 2, 4, 3 ]))
